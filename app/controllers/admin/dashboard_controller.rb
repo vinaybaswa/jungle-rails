2f0d6920f10056f19792e82227518b84e2264747
Vinay Baswa <vinaybaswa@gmail.com>
2020-10-15 19:54:22 +0000

    implement dashboard count for admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USER'], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
+
   def show
+    @categories = Category.order(id: :desc).all
+    @products = Product.order(id: :desc).all
   end
 end
