dc23e2e07986858dc920a1115d50692384812e00
Vinay Baswa <vinaybaswa@gmail.com>
2020-10-15 15:13:19 +0000

    setup baxic auth for admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USER'], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
+  # http_basic_authenticate_with name: 'Jungle', password: 'book'
+
   def index
     @products = Product.order(id: :desc).all
   end
